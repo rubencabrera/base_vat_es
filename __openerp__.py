9dd9c84e550606ca2bf36a6bf5fd7cda4e16145d
Ruben <rubencabrera85@gmail.com>
2015-06-19 08:59:06 +0200

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Spanish VAT Number fake Validation',
+    'version': '0.1',
+    'category': 'Hidden/Dependency',
+    'description': """
+VAT validation skipping
+========================
+
+This module skips Spanish VAT validation so errors don't interrupt migrations
+
+
+    """,
+    'author': 'bisneSmart',
+    'depends': ['account', 'base_vat'],
+    'website': 'http://www.bisnesmart.com',
+   
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
